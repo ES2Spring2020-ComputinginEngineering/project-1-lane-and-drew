69f9df41b73f61d614a4bdfd095ced5adc8de53d
drewmackin1 <drewmacklin1@gmail.com>
2020-03-26 18:20:09 -0400

    Update logger.py

--- Step 3/logger.py
+++ Step 3/logger.py
@@ -1,8 +1,7 @@
 import microbit as mb
-import radio  # Needs to be imported separately
+import radio
 
-# Change the channel if other microbits are interfering. (Default=7)
-radio.on()  # Turn on radio
+radio.on()
 radio.config(channel=11, length=100)
 
 print('Program Started')
@@ -12,16 +11,14 @@ while not mb.button_a.is_pressed():  # wait for button A to be pressed to begin 
     mb.sleep(10)
 
 radio.send('start') # Send the word 'start' to start the receiver
-mb.sleep(1000)
+mb.sleep(1000) #wait one second
 mb.display.show(mb.Image.HEART)  # Display Heart while logging
 
-
-# Read and send accelerometer data repeatedly until button A is pressed again
-while not mb.button_a.is_pressed():
-    time = str(mb.running_time())
-    xaccel = str(mb.accelerometer.get_x())
+while not mb.button_a.is_pressed(): # Read and send accelerometer data repeatedly until button A is pressed again
+    time = str(mb.running_time()) #time = the running time
+    xaccel = str(mb.accelerometer.get_x()) #xacceleration = the x acceleration 
     yaccel = str(mb.accelerometer.get_y())
     zaccel = str(mb.accelerometer.get_z())
-    radio.send(time + "," + xaccel + "," + yaccel + "," + zaccel)
-    mb.sleep(10)
+    radio.send(time + "," + xaccel + "," + yaccel + "," + zaccel) #send the four values as a tuple separated by commas
+    mb.sleep(10) #wait 1/100 of a second before sending new values
     print(xaccel+" "+yaccel+" "+zaccel+" "+ time)
